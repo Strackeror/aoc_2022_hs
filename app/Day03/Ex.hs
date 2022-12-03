d36b366d092b032190b073dc74dcdf49fb29f0ca
Strackeror <rremilavergne+github@gmail.com>
2022-12-03 17:17:46 +0100

    Day03 part2

--- app/Day03/Ex.hs
+++ app/Day03/Ex.hs
@@ -1,48 +1,35 @@
 module Day03.Ex (part1, part2) where
 
-import Control.Arrow ((&&&))
-import Data.Function ((&))
 import Data.List (elemIndex, find, group, minimumBy, sort, sortOn)
-import qualified Data.Map as M
+import Data.List.Split (chunksOf)
 import Data.Maybe (fromJust)
 import Debug.Trace (trace, traceShowId)
 
-data RucksackElem = RucksackElem
-  { char :: Char,
-    p1Count :: Int,
-    p2Count :: Int
-  }
-  deriving (Show)
-
 priority :: Char -> Int
-priority c = case elemIndex c " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" of
-  Nothing -> error "invalid char"
-  Just index -> index
+priority c = fromJust $ elemIndex c " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-counts :: String -> [(Char, Int)]
-counts = map (head &&& length) . group . sort
+organizeByCount :: (Ord k) => [[k]] -> [(k, [Int])]
+organizeByCount k =
+  let keys = map head $ group . sort $ foldl1 (++) k
+      countTuple c = (c, map (length . filter (== c)) k)
+   in map countTuple keys
 
-makeElems :: [Char] -> [RucksackElem]
-makeElems str =
-  let (p1, p2) = splitAt (div (length str) 2) str
-      list =
-        map (\(char, count) -> (char, (count, 0))) (counts p1)
-          ++ map (\(char, count) -> (char, (0, count))) (counts p2)
+isInAll :: (Foldable t, Ord a1, Num a1) => (a2, t a1) -> Bool
+isInAll (_, list) = all (> 0) list
 
-      dict = M.fromListWith (\(a, b) -> \(a2, b2) -> (a + a2, b + b2)) list
-   in map (\(key, (count1, count2)) -> RucksackElem key count1 count2) $ M.toList dict
-
-isInBoth :: RucksackElem -> Bool
-isInBoth RucksackElem {char = _, p1Count = p1, p2Count = p2} | p1 /= 0 && p2 /= 0 = True
-isInBoth _ = False
+splitHalf :: [a] -> [[a]]
+splitHalf str = let (a, b) = splitAt (div (length str) 2) str in [a, b]
 
 part1 :: String -> String
 part1 str =
   show $
     sum $
-      priority . char . fromJust . find isInBoth . makeElems
+      priority . fst . fromJust . find isInAll . organizeByCount . splitHalf
         <$> lines str
 
-
 part2 :: String -> String
-part2 str = ""
+part2 str =
+  show $
+    sum $
+      priority . fst . fromJust . find isInAll . organizeByCount
+        <$> (chunksOf 3 . lines $ str)
