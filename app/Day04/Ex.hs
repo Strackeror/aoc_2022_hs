468468bce54ad3f1d1ecc76e30e511e061ee9019
Strackeror <rremilavergne+github@gmail.com>
2022-12-04 10:54:19 +0100

    Day04

---
+++ app/Day04/Ex.hs
@@ -0,0 +1,36 @@
+module Day04.Ex (part1, part2) where
+
+import Data.List.Split
+
+type Range = ((Int, Int), (Int, Int))
+
+readRangePair :: String -> Range
+readRangePair str =
+  ((a, b), (c, d))
+  where
+    [[a, b], [c, d]] = map (map read . splitOn "-") $ splitOn "," str
+
+checkFullOverlap :: Range -> Bool
+checkFullOverlap ((a, b), (c, d))
+  | a <= c && b >= d = True
+  | c <= a && d >= b = True
+  | otherwise = False
+
+checkAnyOverlap :: Range -> Bool
+checkAnyOverlap ((a, b), (c, d)) = a <= d && c <= b 
+
+part1 :: String -> String
+part1 str =
+  show $
+    length $
+      filter checkFullOverlap $
+        readRangePair
+          <$> lines str
+
+part2 :: String -> String
+part2 str =
+  show $
+    length $
+      filter checkAnyOverlap $
+        readRangePair
+          <$> lines str
--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,14 @@ module Main where
 import Day01.Ex as Day01 ( part1, part2 )
 import Day02.Ex as Day02 ( part1, part2 )
 import Day03.Ex as Day03 ( part1, part2 )
+import Day04.Ex as Day04 ( part1, part2 )
 import System.Environment ( getArgs )
 
 days =
   [ (Day01.part1, Day01.part2),
     (Day02.part1, Day02.part2),
-    (Day03.part1, Day03.part2)
+    (Day03.part1, Day03.part2),
+    (Day04.part1, Day04.part2)
   ]
 
 main :: IO ()
