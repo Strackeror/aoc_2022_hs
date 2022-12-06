fbd03c21ec35d5079ed339e423b047339914dc3f
Strackeror <rremilavergne@gmail.com>
2022-12-06 19:41:55 +0100

    golfing day06

--- app/Day06/Ex.hs
+++ app/Day06/Ex.hs
@@ -1,22 +1,13 @@
 module Day06.Ex (part1, part2) where
 
-import qualified Data.Set as Set
-import Data.List.Split
+import Data.List (nub)
 
-allDiff array = Set.size (Set.fromList array) == length array
-
-splitAtNCommon count array =
-  let first = head array
-      (firstN, rest) = splitAt count array
-  in
-    if allDiff firstN then
-      (firstN, rest)
-    else
-      let (before, after) = splitAtNCommon count (tail array) in (first:before, after)
-
+countFirstNCommon count array 
+  | (nub . take count $ array) == take count array = count
+  | otherwise = countFirstNCommon count (tail array) + 1
 
 part1 :: String -> String
-part1 = show . length . fst . splitAtNCommon 4
+part1 = show . countFirstNCommon 4
 
 part2 :: String -> String
-part2 = show . length . fst . splitAtNCommon 14
+part2 = show . countFirstNCommon 14
