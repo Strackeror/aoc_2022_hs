6017ba3afe32909b29d2a5358b77f77ae4ef13d6
Strackeror <rremilavergne@gmail.com>
2022-12-08 23:28:01 +0100

    Day08 part 2

--- app/Day08/Ex.hs
+++ app/Day08/Ex.hs
@@ -1,41 +1,51 @@
 module Day08.Ex (part1, part2) where
 
-import Data.List (maximumBy, nub, transpose)
+import Data.List (elemIndex, findIndex, maximumBy, nub, tails, transpose)
 import Data.List.Split (chunksOf)
 import Data.Ord (comparing)
-import Debug.Trace (traceShowId, traceShow)
+import Debug.Trace (traceShow, traceShowId)
 
 readInput :: String -> [[Int]]
 readInput = map (map read . chunksOf 1) . lines
 
-maxIndex :: [Int] -> Int
-maxIndex = fst . maximumBy (comparing snd) . zip [0 ..]
-
-visibleFilter :: [Int] -> [Bool]
-visibleFilter = recurse (-1)
+visibleFromStart :: [Int] -> [Bool]
+visibleFromStart = recurse (-1)
   where
     recurse _ [] = []
     recurse max (x : xs)
       | x > max = True : recurse x xs
       | otherwise = False : recurse max xs
 
-visibleMatrix = map visibleFilter
+countUntil :: (a -> Bool) -> [a] -> Int
+countUntil _ [] = 0
+countUntil f (x : xs)
+  | f x = 1
+  | otherwise = 1 + countUntil f xs
 
-reverseVisibleMatrix = map (reverse . visibleFilter . reverse)
+visibleAfter :: [Int] -> [Int]
+visibleAfter [] = []
+visibleAfter (x : xs) = countUntil (>= x) xs : visibleAfter xs
 
-transposedMatrix = transpose . visibleMatrix . transpose
+revMap f = map (reverse . f . reverse)
 
-reverseTransposedMatrix = transpose . reverseVisibleMatrix . transpose
+transposeMap f = transpose . map f . transpose
 
-mergeMatrix :: [[Bool]] -> [[Bool]] -> [[Bool]]
-mergeMatrix = zipWith $ zipWith (||)
+revTransposeMap f = transpose . revMap f . transpose
 
-part1 str =
-  let matrix = readInput str
-   in show $
-        sum $
-          map (length . filter id) $
-              foldl1 mergeMatrix $
-                map ($ matrix) [visibleMatrix, reverseVisibleMatrix, transposedMatrix, reverseTransposedMatrix]
+mapMatrix4Dirs f matrix = [map f matrix, revMap f matrix, transposeMap f matrix, revTransposeMap f matrix]
 
-part2 _ = ""
+part1 =
+  show
+    . sum
+    . map (length . filter id)
+    . foldl1 (zipWith $ zipWith (||))
+    . mapMatrix4Dirs visibleFromStart
+    . readInput
+
+part2 =
+  show
+    . maximum
+    . map maximum
+    . foldl1 (zipWith $ zipWith (*))
+    . mapMatrix4Dirs visibleAfter
+    . readInput
